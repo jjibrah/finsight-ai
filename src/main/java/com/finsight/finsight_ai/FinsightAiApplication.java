ae0cd5b6119275d95a8f59bc610576f67e8dfc15
Abdifatah Ibrahim <121964336+jjibrah@users.noreply.github.com>
2026-06-08 21:42:43 +0800

    Sprint 1: project setup + holding entity + repository

---
+++ src/main/java/com/finsight/finsight_ai/FinsightAiApplication.java
@@ -0,0 +1,13 @@
+package com.finsight.finsight_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinsightAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinsightAiApplication.class, args);
+	}
+
+}
